a4ba968eb6f2eb6fb184f4ef8f59d31c6e63f087
shinXan <nishit.gupta6708@gmail.com>
2019-10-04 09:58:16 +0530

    modify PointerDrawable

--- learning-arcore/SceneformIntro/app/src/main/java/com/example/sceneformintro/PointerDrawable.java
+++ learning-arcore/SceneformIntro/app/src/main/java/com/example/sceneformintro/PointerDrawable.java
@@ -1,6 +1,11 @@
 package com.example.sceneformintro;
 
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 
 public class PointerDrawable extends Drawable {
+    private final Paint paint = new Paint();
+    private boolean enabled;
+
+    
 }
