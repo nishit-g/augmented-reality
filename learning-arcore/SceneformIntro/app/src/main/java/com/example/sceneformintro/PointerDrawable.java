d40383ea3492d254bc3fd3b9e4e26ca4d569e438
shinXan <nishit.gupta6708@gmail.com>
2019-10-05 10:04:31 +0530

    modify PointerDrawable

--- learning-arcore/SceneformIntro/app/src/main/java/com/example/sceneformintro/PointerDrawable.java
+++ learning-arcore/SceneformIntro/app/src/main/java/com/example/sceneformintro/PointerDrawable.java
@@ -1,6 +1,7 @@
 package com.example.sceneformintro;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
@@ -25,7 +26,15 @@ public class PointerDrawable extends Drawable {
 
     @Override
     public void draw(@NonNull Canvas canvas) {
-
+        float cx = canvas.getWidth()/2;
+        float cy = canvas.getHeight()/2;
+        if (enabled) {
+            paint.setColor(Color.GREEN);
+            canvas.drawCircle(cx, cy, 10, paint);
+        } else {
+            paint.setColor(Color.GRAY);
+            canvas.drawText("X", cx, cy, paint);
+        }
     }
 
     @Override
