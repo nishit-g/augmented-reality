048434529573117c79720524c545e4ecfcc6c919
shinXan <nishit.gupta6708@gmail.com>
2020-01-17 15:26:18 +0530

    add second line

--- learning-arcore/Checking.java
+++ learning-arcore/Checking.java
@@ -1,5 +1,6 @@
 public class Checking{
     public static void main(String[] args){
         System.out.println("Github is working just fine, I'll learn it ");
+        System.out.println("Checking the command line");
     }
 }
