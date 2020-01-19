a8c69606e74d5a7f45092b8d9a0a481419e3e5e1
shinXan <32142878+shinXan@users.noreply.github.com>
2020-01-19 17:37:55 +0530

    add  newScene

---
+++ learning-unity/Rookie/Rookie/Assets/RookieI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RookieI : MonoBehaviour
+{
+
+   
+    //Create a gameObject for having a reference to change the color
+    public GameObject cube;
+
+    void Start()
+    {
+        //Get the color component of the cube
+        cube.GetComponent<Renderer>().material.color = Color.red;
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("This isn't supposed to work");
+        }
+
+    }
+}
